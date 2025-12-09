e20c78cfbaf793e155097b49abe09e2a4116bf48
ricxi <irixoc@gmail.com>
2025-12-08 23:52:07 -0500

    organized tutorial components

--- Assets/Scripts/HintCollider.cs
+++ Assets/Scripts/HintCollider.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class HintCollider : MonoBehaviour
 {
     [SerializeField] private string helpText;
+    [SerializeField] private GameObject visualHint;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        PlayerUIHandler.Instance.DisplayTextToPlayer(helpText, 2f);
-        // PlayerUIHandler.Instance.DisplayTextToPlayer("Press \'X\' to interact with the gate.", 2f);
+        if (collision.CompareTag("Player"))
+        {
+            PlayerUIHandler.Instance.DisplayTextToPlayer(helpText, 2f);
+
+            if (visualHint != null)
+            {
+                visualHint.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (visualHint != null)
+            {
+                visualHint.SetActive(false);
+            }
+        }
     }
 }
